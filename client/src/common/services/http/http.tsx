c04b95e72d5c2f19a7afdb80fda665ec2962a5c5
kamilsoloducha <kamilsoloducha@gmail.com>
2021-11-09 21:55:21 +0100

    update api-path

--- client/src/common/services/http/http.tsx
+++ client/src/common/services/http/http.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const API_PATH = "http://localhost:5000";
+// export const API_PATH = "http://localhost:5000";
+export const API_PATH = "https://wordki-server.herokuapp.com";
 
 const instance = axios.create({
   baseURL: API_PATH,
