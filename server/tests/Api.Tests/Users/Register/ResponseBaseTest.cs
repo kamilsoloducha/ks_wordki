4cebc6f7cd4bd0f7bede172e7f181e8a7cf549f5
Kamil <kamilsoloducha@gmail.com>
2022-08-25 21:22:29 +0200

    e2e test reactivation

--- server/tests/Api.Tests/Users/Register/ResponseBaseTest.cs
+++
@@ -1,9 +0,0 @@
-namespace Api.Tests.Users
-{
-    public class ResponseBaseTest<T>
-    {
-        public T Response { get; set; }
-        public string Error { get; set; }
-        public bool IsCorrect { get; set; }
-    }
-}
