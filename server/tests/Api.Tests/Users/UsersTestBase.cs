a57d4543ae751f25b3cc13c105c04297e1cd3e48
Kamil <kamilsoloducha@gmail.com>
2022-08-25 21:22:47 +0200

    test

--- server/tests/Api.Tests/Users/UsersTestBase.cs
+++ server/tests/Api.Tests/Users/UsersTestBase.cs
@@ -1,14 +1,17 @@
-// using System.Threading.Tasks;
-// using Wordki.Tests.E2E.Feature;
+using System.Threading.Tasks;
+using Api.Tests.Model.Users;
+using Microsoft.EntityFrameworkCore;
+using Wordki.Tests.E2E.Feature;
 
-// namespace Api.Tests.Users
-// {
-//     public class UsersTestBase : TestBase
-//     {
-//         protected async Task ClearUsersSchema()
-//         {
-//             using var dbContext = new TestDbContext();
-//             await dbContext.CleanUsersSchema();
-//         }
-//     }
-// }
+namespace Api.Tests.Users
+{
+    public class UsersTestBase : TestBase
+    {
+        protected async Task ClearUsersSchema()
+        {
+            await using var dbContext = new UsersContext();
+            await dbContext.Database.ExecuteSqlRawAsync("Delete from users.\"Roles\"");
+            await dbContext.Database.ExecuteSqlRawAsync("Delete from users.\"Users\"");
+        }
+    }
+}
