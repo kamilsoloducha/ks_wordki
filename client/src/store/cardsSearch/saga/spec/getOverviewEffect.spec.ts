4ea4dfeb67b164b817943c680c69149db9a19831
Kamil <kamilsoloducha@gmail.com>
2022-10-11 22:12:40 +0200

    fix tests

--- client/src/store/cardsSearch/saga/spec/getOverviewEffect.spec.ts
+++ client/src/store/cardsSearch/saga/spec/getOverviewEffect.spec.ts
@@ -1,10 +1,10 @@
 import "test/matcher/toDeepEqual";
 import * as cards from "api/services/cards";
 import { SagaIterator } from "redux-saga";
-import { call, put, select, take } from "redux-saga/effects";
+import { call, put, select, take, takeEvery } from "redux-saga/effects";
 import { selectUserId } from "store/user/selectors";
 import { getOverview, getOverviewSuccess } from "store/cardsSearch/reducer";
-import { getOverviewEffect } from "../getOverviewEffect";
+import { getOverviewEffect, getOverviewWorker } from "../getOverviewEffect";
 import { CardsOverview } from "pages/cardsSearch/models";
 
 describe("getOverviewEffect", () => {
@@ -21,12 +21,6 @@ describe("getOverviewEffect", () => {
   });
 
   it("should go through", () => {
-    const action = getOverview();
-    const userId = "userId";
-    const response: CardsOverview = { all: 1, drawers: [1, 2, 3], lessonIncluded: 2, ticked: 3 };
-    expect(saga.next().value).toStrictEqual(take("cardsSearch/getOverview"));
-    expect(saga.next(action).value).toStrictEqual(select(selectUserId));
-    expect(saga.next(userId).value).toStrictEqual(call(mock, userId));
-    expect(saga.next(response).value).toDeepEqual(put(getOverviewSuccess({ overview: response })));
+    expect(saga.next().value).toStrictEqual(takeEvery("cardsSearch/getOverview", getOverviewWorker));
   });
 });
