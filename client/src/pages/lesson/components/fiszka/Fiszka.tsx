b01a00b09b65b6956beeda0f1ecdb8397c79432c
Kamil <kamilsoloducha@gmail.com>
2024-03-04 22:01:50 +0100

    fiszki component improvement (#15)

--- client/src/pages/lesson/components/fiszka/Fiszka.tsx
+++ client/src/pages/lesson/components/fiszka/Fiszka.tsx
@@ -1,62 +1,142 @@
-import * as key from "../../models/keyCodes";
-import * as resultTypes from "../../models/resultTypes";
-import * as actions from "store/lesson/reducer";
-import * as sel from "store/lesson/selectors";
-import { ReactElement, useCallback, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import Answer from "../answer/Answer";
-import Question from "../question/Question";
+import * as key from '../../models/keyCodes'
+import * as resultTypes from '../../models/resultTypes'
+import * as actions from 'store/lesson/reducer'
+import * as sel from 'store/lesson/selectors'
+import React, { ReactElement, useCallback, useEffect, useMemo, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import Answer from '../answer/Answer'
+import Question from '../question/Question'
 
 export default function Fiszka(): ReactElement {
-  const dispatch = useDispatch();
-  const status = useSelector(sel.selectLessonState);
-  const currectRepeat = useSelector(sel.selectCurrectRepeat);
+  const dispatch = useDispatch()
+  const status = useSelector(sel.selectLessonState)
+  const currectRepeat = useSelector(sel.selectCurrectRepeat)
+  const [clickPosition, setClickPosition] = useState<number | undefined>(undefined)
+  const [clickAndDragDelata, setClickAndDragDelta] = useState<number | undefined>(undefined)
+  const [clickTimestamp, setClickTimestamp] = useState<number>(0)
+
+  const questionElement = useMemo(
+    () =>
+      currectRepeat && (
+        <Question
+          value={currectRepeat.question}
+          example={currectRepeat.questionExample}
+          language={1}
+        />
+      ),
+    [currectRepeat]
+  )
+
+  const answerElement = useMemo(
+    () =>
+      currectRepeat && (
+        <Answer
+          isVisible={status.answer}
+          correctAnswer={currectRepeat.answer}
+          exampleAnswer={currectRepeat.answerExample}
+          userAnswer={currectRepeat.answer}
+        />
+      ),
+    [currectRepeat, status]
+  )
 
   const handleEventEvent = useCallback(
     (event: KeyboardEvent) => {
       switch (event.key) {
         case key.Enter: {
-          dispatch(actions.check());
-          break;
+          dispatch(actions.check())
+          break
         }
         case key.Left: {
-          dispatch(actions.wrong({ result: -1 }));
-          break;
+          dispatch(actions.wrong({ result: resultTypes.Wrong }))
+          break
         }
         case key.Right: {
-          dispatch(actions.correct({ result: resultTypes.Correct }));
-          break;
+          dispatch(actions.correct({ result: resultTypes.Correct }))
+          break
         }
       }
     },
     [dispatch]
-  );
+  )
 
   useEffect(() => {
-    document.addEventListener("keydown", handleEventEvent);
+    document.addEventListener('keydown', handleEventEvent)
     return () => {
-      document.removeEventListener("keydown", handleEventEvent);
-    };
-  }, [handleEventEvent]);
+      document.removeEventListener('keydown', handleEventEvent)
+    }
+  }, [handleEventEvent])
 
   if (!status.card || !currectRepeat) {
-    return <></>;
+    return <></>
   }
 
+  const onUntouch = () => {
+    let percentage = clickAndDragDelata
+      ? Math.abs(clickAndDragDelata / document.body.clientWidth)
+      : 0
+    percentage = percentage > 1 ? 1 : percentage
+
+    if (percentage > 0.4) {
+      if (clickAndDragDelata! > 0) {
+        dispatch(actions.correct({ result: resultTypes.Correct }))
+      } else {
+        dispatch(actions.wrong({ result: resultTypes.Wrong }))
+      }
+    }
+
+    setClickAndDragDelta(undefined)
+    setClickPosition(undefined)
+  }
+
+  const onFingerMove = (event: React.TouchEvent<HTMLDivElement>) => {
+    if (!!!clickPosition) {
+      return
+    }
+
+    if (status.btnCheck) {
+      return
+    }
+
+    setClickAndDragDelta(event.touches[0].clientX - clickPosition)
+  }
+
+  const onTouch = (event: React.TouchEvent<HTMLDivElement>) => {
+    setClickPosition(event.touches[0].clientX)
+
+    if (event.timeStamp - clickTimestamp < 300) {
+      if (status.btnCheck) {
+        dispatch(actions.check())
+      }
+    }
+    setClickTimestamp(event.timeStamp)
+  }
+
+  let percentage = clickAndDragDelata ? Math.abs(clickAndDragDelata / document.body.clientWidth) : 0
+  percentage = percentage > 1 ? 1 : percentage
+
   return (
-    <div>
-      <Question
-        value={currectRepeat.question}
-        example={currectRepeat.questionExample}
-        language={1}
-      />
+    <div
+      className="relative"
+      onTouchStart={onTouch}
+      onTouchEnd={onUntouch}
+      onTouchMove={onFingerMove}
+    >
+      {clickAndDragDelata && clickAndDragDelata < 0 && (
+        <div
+          className="z-10 absolute w-full h-full bg-gradient-to-r from-red-500"
+          style={{ opacity: percentage }}
+        ></div>
+      )}
+      {clickAndDragDelata && clickAndDragDelata > 0 && (
+        <div
+          className="z-10 absolute w-full h-full bg-gradient-to-l from-green-500"
+          style={{ opacity: percentage }}
+        ></div>
+      )}
+      {questionElement}
       <hr />
-      <Answer
-        isVisible={status.answer}
-        correctAnswer={currectRepeat.answer}
-        exampleAnswer={currectRepeat.answerExample}
-        userAnswer={currectRepeat.answer}
-      />
+      {answerElement}
     </div>
-  );
+  )
 }
--- client/src/pages/lesson/components/question/Question.tsx
+++ client/src/pages/lesson/components/question/Question.tsx
@@ -1,21 +1,17 @@
-import "./Question.scss";
-import { ReactElement } from "react";
+import './Question.scss'
+import { ReactElement } from 'react'
 
-export default function Question({
-  value,
-  language,
-  example,
-}: Model): ReactElement {
+export default function Question({ value, language, example }: Model): ReactElement {
   return (
     <div className="question-main-container">
       <div className="question-value">{value}</div>
       <div className="question-example">{example}</div>
     </div>
-  );
+  )
 }
 
 interface Model {
-  value: string;
-  language: number;
-  example?: string;
+  value: string
+  language: number
+  example?: string
 }
